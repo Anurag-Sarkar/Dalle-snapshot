09850fca86fd9cb1d41526ad5503794445eca82d
unknown <sarkaranurag104@gmail.com>
2022-12-24 11:06:57 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
-import { Fragment } from "react"
-import { useState } from "react"
+import { useState,useEffect,useRef } from "react"
+import "./assets/stylesheets/style.css"
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+	apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 
 function App() {
-  const [title, setTitile] = useState("")
-  const [author, setAuthor] = useState("")
-  const [url, setUrl] = useState("")
-  const [gallery, setGallery] = useState([])
+  const button = useRef(null)
+  const [Images, setImages] = useState([])
+  const [Prompt, setPrompt] = useState("A picture of horse on human")
+  const [State, setState] = useState("generated")
+  const [Toogle, setToogle] = useState(false)
+  
+  
+  useEffect(()=>{
+    setState("generating")
+    openai.createImage({
+      prompt: Prompt,
+      n: 3,
+      size: "512x512",})
+      .then((res)=>{
+        setState("generated")
+        var arr = []
+        res.data.data.map((item)=>{
+          arr.push(item.url)
+        })
+        setImages(arr)
+        console.log(arr)
+      })
+      .catch(err=>{
+        console.log(err)
+        button.current.innerHTML = "Requsted Image can't be Generated"
+      })
 
-  const submitHandler = (data) => {
-    data.preventDefault()
-    if (title.trim() && author.trim() && url.trim()) {
-      console.log(title, author, url)
-      var deta = { "title": title, "author": author, "url": url }
-      setGallery([...gallery, deta])
-      console.log(gallery)
-      setAuthor("")
-      setTitile("")
-      setUrl("")
+  },[Toogle])
 
-    }
-    else {
-      alert("Bhaag jaa bsdk")
-
-    }
-  }
-  var cardlist = "lol"
-
-  if(gallery.length > 0){
-    cardlist = gallery.map((card,index)=>(
-      <div key={index} className="card" style={{ width: "300px" }}>
-        <img src={card.url} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{card.title}</h5>
-          <p className="card-text">{card.author}</p>
-        </div>
-      </div>
-    ))
-  }
-
-  return (
+ return (
     <>
-      <form action="" onSubmit={submitHandler} className="container w-25">
-        <input type="text" onChange={(e) => setTitile(e.target.value)} className="form-control mt-5" placeholder="title" value={title} name="title" />
-        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className="form-control mt-2" placeholder="author" name="author" />
-        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} className="form-control mt-2" placeholder="image" name="image" />
-        <button className="btn btn-success mt-4">Submit</button>
-      </form>
-      <div className="container d-flex mt-5">
-      {cardlist}
-      </div>
-      
+     <div id="main">
+        <div id="content">
+            <div id="text">
+                <h1>SnapShot</h1>
+                <input onChange={(e)=>{setPrompt(e.target.value)}} value={Prompt} type="text" placeholder="Type naything"/>
+                <p ref={button} use onClick={()=>{setToogle(!Toogle)}}>{State === "generated"?"Generate":"Generating..."}</p>
+                <h2>Pictures of {Prompt}</h2>
+            </div>
+            <div id="images">
+              {Images.map((item,index)=>(
+                  <div key={index} id="img">
+                    <img src={item} alt={item}/>
+                  </div>
+                ))}
+            </div>
+        </div>
+    </div>
     </>
   );
 }
